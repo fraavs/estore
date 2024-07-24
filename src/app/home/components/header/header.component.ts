0f5b1307aac08516da91a157cd69a3d10620b5c2
fraavs <ftcinfraavs@gmail.com>
2024-07-24 12:38:08 +0900

    created productdetail page.

--- src/app/home/components/header/header.component.ts
+++ src/app/home/components/header/header.component.ts
@@ -1,6 +1,9 @@
 import { Component, EventEmitter, Output } from '@angular/core';
 import { CategoriesStoreItem } from '../../services/category/categories.storeItem';
 import { SearchKeyword } from '../../types/searchKeyword.type';
+import { NavigationEnd, Router } from '@angular/router';
+import { every, filter } from 'rxjs';
+
 
 @Component({
   selector: 'app-header',
@@ -13,7 +16,13 @@ export class HeaderComponent {
 
   searchClicked: EventEmitter<SearchKeyword> = new EventEmitter<SearchKeyword>();
 
-  constructor(public categoryStore: CategoriesStoreItem) { }
+  displaySearch: boolean = true;
+
+  constructor(public categoryStore: CategoriesStoreItem, private router: Router) {
+    router.events.pipe(filter(event => event instanceof NavigationEnd)).subscribe(event => {
+      this.displaySearch = (event as NavigationEnd).url === '/home/products' ? true : false;
+    })
+  }
 
   onClickSearch(keyword: string, categoryId: string): void {
     this.searchClicked.emit({
--- src/app/home/components/productdetails/productdetails.component.ts
+++ src/app/home/components/productdetails/productdetails.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { ProductsService } from '../../services/product/products.service';
+import { Product } from '../../types/products.type';
+import { Subscription } from 'rxjs';
+
 
 @Component({
   selector: 'app-productdetails',
   templateUrl: './productdetails.component.html',
   styleUrls: ['./productdetails.component.css']
 })
-export class ProductdetailsComponent {
+export class ProductdetailsComponent implements OnInit, OnDestroy {
+  product: Product;
+  subscriptions: Subscription = new Subscription();
 
+
+  constructor(private activateRoute: ActivatedRoute, private productsService: ProductsService) { }
+
+  ngOnInit(): void {
+    const id: number = Number(this.activateRoute.snapshot.paramMap.get('id'));
+    this.subscriptions.add(
+      this.productsService.getProduct(id).subscribe(product => {
+        this.product = product[0];
+      })
+    );
+  }
+  
+  ngOnDestroy(): void {
+    this.subscriptions.unsubscribe();
+  }
 }
