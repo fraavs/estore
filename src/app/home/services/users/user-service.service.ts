0d650e473b22e5bdf61faf25cb8b497a86822ec8
fraavs <ftcinfraavs@gmail.com>
2024-07-25 18:27:02 +0900

    added logout functionality.

--- src/app/home/services/users/user-service.service.ts
+++ src/app/home/services/users/user-service.service.ts
@@ -1,15 +1,18 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Observable, BehaviorSubject } from 'rxjs';
+import { Observable, BehaviorSubject, last } from 'rxjs';
 import { loginToken, user, loggedInUser } from '../../types/user.type';
 
 
 @Injectable()
 export class UserService {
+  private autoLogoutTimer: any;
   private isAuthenticated: BehaviorSubject<boolean> = new BehaviorSubject(false);
 
   private loggedInUserInfo: BehaviorSubject<loggedInUser> = new BehaviorSubject(<loggedInUser>{});
-  constructor(private httpClient: HttpClient) { }
+  constructor(private httpClient: HttpClient) {
+    this.loadToken();
+  }
 
   get isUserAuthenticated(): boolean {
     return this.isAuthenticated.value;
@@ -34,6 +37,7 @@ export class UserService {
   }
 
   activateToken(token: loginToken): void {
+    // token.expiresInSeconds = 10;
     localStorage.setItem('token', token.token);
     localStorage.setItem('expiry', new Date(Date.now() + token.expiresInSeconds * 1000).toISOString())
     localStorage.setItem('username', token.user.username);
@@ -42,11 +46,46 @@ export class UserService {
 
     this.isAuthenticated.next(true);
     this.loggedInUserInfo.next(token.user);
+    this.setAutoLogoutTimer(token.expiresInSeconds * 1000);
   }
 
   logout(): void {
     localStorage.clear();
     this.isAuthenticated.next(false);
     this.loggedInUserInfo.next(<loggedInUser>{});
+    clearTimeout(this.autoLogoutTimer);
+  }
+
+  private setAutoLogoutTimer(duration: number): void {
+    this.autoLogoutTimer = setTimeout(() => {
+      this.logout();
+    }, duration);
+  }
+
+  loadToken(): void {
+    const token: string | null = localStorage.getItem('token');
+    const expiry: string | null = localStorage.getItem('expiry');
+    if (!token || !expiry) {
+      return;
+    } else {
+      const expiresIn: number = new Date(expiry).getTime() - new Date().getTime();
+      if (expiresIn > 0) {
+        const username: string | null = localStorage.getItem('username');
+        const firstName: string | null = localStorage.getItem('firstName');
+        const lastName: string | null = localStorage.getItem('lastName');
+
+        const user: loggedInUser = {
+          username: username !== null ? username : '',
+          firstName: firstName !== null ? firstName : '',
+          lastName: lastName !== null ? lastName : '',
+        }
+
+        this.isAuthenticated.next(true);
+        this.loggedInUserInfo.next(user);
+        this.setAutoLogoutTimer(expiresIn);
+      } else {
+        this.logout();
+      }
+    }
   }
 }
